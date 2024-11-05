aa99bbe1db9121fa1dde77618e04e9781f933db8
Lucas Silva <lucknroll66@outlook.com>
2024-11-05 10:46:29 -0300

    Update UserService.java

--- todosimple/src/main/java/com/luck/todosimple/services/UserService.java
+++ todosimple/src/main/java/com/luck/todosimple/services/UserService.java
@@ -2,8 +2,6 @@ package com.luck.todosimple.services;
 
 import java.util.Optional;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
